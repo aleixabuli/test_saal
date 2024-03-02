4f4f12e832f006de637e4d22b36bdbf81a1c130e
aleixabuli <aleixabuli@gmail.com>
2024-03-02 20:36:33 +0100

    backend: create initial webapi project + prepare urls for deploy to AWS EC2 server

---
+++ Backend/test_backend_saal/Infra.FoodDelivery.Api/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Infra.FoodDelivery.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            
+        }
+
+        /// <summary>
+        /// This method is created for testing
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Test")]
+        public async Task<IActionResult> Test()
+        {
+            string[] array = { "test1", "test2", "test3" };
+            return Ok(array);
+        }
+    }
+}
---
+++ Backend/test_backend_saal/Infra.FoodDelivery.Api/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Urls.Add("http://localhost:5024");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.Urls.Add("http://192.168.1.137:5024");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.Urls.Add("http://172.31.24.215:5024");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
